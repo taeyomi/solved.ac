0582be5234ba4e184c32b18d88f63522b517c709
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2023-09-20 15:04:43 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main() {
+
+    int A,B;
+
+    std::cout << "";
+    std::cin >> A >> B;
+
+    int sum = A+B;
+
+    std::cout << sum;
+    
+   
+
+    return 0;
+
+
+}
