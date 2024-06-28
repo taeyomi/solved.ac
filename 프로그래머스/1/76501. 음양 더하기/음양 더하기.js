3e9e23601d90737e090b426fc0d39fb1d8b13bcd
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2024-06-28 17:55:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,13 @@
+function solution(absolutes, signs) {
+    let sum = 0;
+    for(let i=0; i<signs.length; i++){
+           if(signs[i]){
+               sum+=absolutes[i];
+           }else{
+               sum += -absolutes[i];
+           }
+    }
+    
+    
+    return sum;
+}
