41f9f694a709e8170ec13fabdb471fa127bfb315
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2024-07-05 19:53:57 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,9 @@
+function solution(phone_number) {
+    var answer = phone_number.split('');;
+ 
+    for(let i =0; i<phone_number.length-4; i++){
+        answer[i]='*';
+    }
+  
+    return answer.join('');
+}
