19b1022b5acbac785a0cda3ebb0a90bdc8d6227d
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2024-08-13 16:59:13 +0900

    [level 1] Title: 최소직사각형, Time: 7.69 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,13 @@
+function solution(sizes) {
+    const arr = sizes.map(([w,h]) => w<h ? [h,w] : [w,h]);
+    
+    let maxSize = [0,0];
+    
+    arr.forEach(([w,h]) => {
+           if (w > maxSize[0]) maxSize[0] = w;
+           if (h > maxSize[1]) maxSize[1] = h;
+
+    })
+     return maxSize[0]*maxSize[1];
+
+}
