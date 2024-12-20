fb21b4011d922a5aeea8cfcba1171df859770d17
taeyoung Kim <100909703+taeyomi@users.noreply.github.com>
2024-12-20 18:52:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,10 @@
+function solution(n, arr1, arr2) {
+    var result = [];
+    
+    for(let i = 0; i<n; i++){
+          let num = (arr1[i] | arr2[i]).toString(2);
+          num = num.replace(/1/g, '#').replace(/0/g, " ");
+          result.push(num.padStart(n, " "));
+    }
+    return result;
+}
